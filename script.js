0c5ae0ce2b6e3f021d0560c6faa4fab482d58a73
Edson Rodrigues <thecodeverse12@gmail.com>
2023-12-05 22:39:26 +0100

    :tada: Initial Commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function toggleMode(){
+  // Selecionando elementos do documento
   const html = document.documentElement;
   const imagem = document.querySelector('#profile img');
+  //Mudando de default para light com a metodo toggle
   html.classList.toggle('light');
+  //Alterando imagem em funcao do tema
   if(html.classList.contains('light')){
     imagem.setAttribute('src', './assets/avatar-light.png');
   }else{
